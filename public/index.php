a45cb0214b8b7c84af8d9f6023998d05c4c584cc
August <hackerjef@users.noreply.github.com>
2019-12-05 05:12:05 -0500

    WIP REDIS/SENDING MESSAGES/BOTSTUFF

--- public/index.php
+++ public/index.php
@@ -1,32 +1,49 @@
 <?php
     require_once __DIR__ . '/../vendor/autoload.php';
     use Symfony\Component\Yaml\Yaml;
-    use \DiscordWebhooks\Client;
-    use \DiscordWebhooks\Embed;
+    use RestCord\DiscordClient;
+    use RedisClient\RedisClient;
+    use RedisClient\Client\Version\RedisClient2x6;
+    use RedisClient\ClientFactory;
 
-    function($config, $embed) {
-        $webhook = new Client($config["discord"]["url"]);
-        $webhook->username($config["discord"]["username"])->avatar($config['webserver']['target_server'] . "/src/" . $config['discord']['avatar_filename'])->embed($embed)->send();
-    };
+    #config
+    try {
+        $config = Yaml::parseFile(__DIR__ . '/../settings.yaml');
+    } catch (ParseException $exception) {
+        printf('Unable to parse the YAML string: %s', $exception->getMessage());
+    }
+
+    $Redis = ClientFactory::create($config["redis"]);
+    $discord = new DiscordClient(['token' => $config["discord"]["token"]]);
+    # Figure out what to do next lol
+    //var_dump($discord->channel->createMessage(['channel.id' => 521181423512846357, 'content' => 'This is a test.jpg']));
 
     class event_router {
         public function boards() {
             echo "boards";
+            exit;
         }
 
-        public function column() {
-            echo "column";
+        public function columns() {
+            echo "columns";
+            exit;
         }
 
         public function cards() {
             echo "cards";
+            exit;
         }
 
         public function comments() {
             echo "comments";
+            exit;
+        }
+
+        public function invalid() {
+            header("HTTP/1.0 404 Invalid event");
+            exit;
         }
     }
-
 
     # only allow post requests
     if ($_SERVER['REQUEST_METHOD'] != 'POST') {
@@ -40,13 +57,6 @@
         exit;
     }
 
-
-    try {
-        $config = Yaml::parseFile(__DIR__ . '/../settings.yaml');
-    } catch (ParseException $exception) {
-        printf('Unable to parse the YAML string: %s', $exception->getMessage());
-    }
-
     #Check header exists, if so check hash
     $hash = "sha1=" . hash_hmac('sha1', file_get_contents('php://input'), $config["glo"]["secret"]);
     if ($config["glo"]["check_secret"] and $hash != getallheaders()['x-gk-signature']) {
@@ -57,7 +67,6 @@
     if (method_exists('event_router', getallheaders()['x-gk-event'])) {
         [new event_router(), getallheaders()['x-gk-event']]();
     } else {
-        header("HTTP/1.0 404 Invalid event");
-        exit;
+        [new event_router(), "invalid"]();
     }
 ?>
