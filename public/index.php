6c951f5741405ad6c802baead6b6af89c70e2b66
August <hackerjef@users.noreply.github.com>
2019-12-12 19:24:22 -0500

    read description tldr: Fuck
    1.updated redis task to allow dev contained within env
    2. removed stuff that makes no sence
    3. changed old redis lib to use predis
    4. worked on updating/sending msgs
    
    Todo: Work on way to track webhooks/data  :)

--- public/index.php
+++ public/index.php
@@ -2,46 +2,27 @@
     require_once __DIR__ . '/../vendor/autoload.php';
     use Symfony\Component\Yaml\Yaml;
     use RestCord\DiscordClient;
-    use RedisClient\RedisClient;
-    use RedisClient\Client\Version\RedisClient2x6;
-    use RedisClient\ClientFactory;
 
     #config
     try {
         $config = Yaml::parseFile(__DIR__ . '/../settings.yaml');
     } catch (ParseException $exception) {
         printf('Unable to parse the YAML string: %s', $exception->getMessage());
-    }
+    };
 
-    $Redis = ClientFactory::create($config["redis"]);
-    $discord = new DiscordClient(['token' => $config["discord"]["token"]]);
-    # Figure out what to do next lol
-    //var_dump($discord->channel->createMessage(['channel.id' => 521181423512846357, 'content' => 'This is a test.jpg']));
+    $data = json_decode(file_get_contents('php://input'));
 
-    class event_router {
-        public function boards() {
-            echo "boards";
-            exit;
-        }
-
-        public function columns() {
-            echo "columns";
-            exit;
-        }
-
-        public function cards() {
-            echo "cards";
-            exit;
-        }
-
-        public function comments() {
-            echo "comments";
-            exit;
-        }
-
-        public function invalid() {
-            header("HTTP/1.0 404 Invalid event");
-            exit;
+    $client = new Predis\Client();
+    $redis = new Predis\Client($config["redis-client"]);
+    $discord = new DiscordClient($config["discord-client"]);
+    function send_msg($embed) {
+        //key:    type:card_id:msg_id
+        //ext:    5ddaf7385b8585000f203e13:
+        if ($redis->get(getallheaders()['x-gk-event'] . ':' . $eventid) == False) {
+            $msg = $discord->channel->createMessage(['channel.id' => $config['discord']["channel_id"], 'embed' => $embed]);
+            $redis->set(getallheaders()['x-gk-event'] . ':' .  $eventid, $msg['id']);
+        } else {
+            $discord->channel->editMessage(['channel.id' => $config['discord']["channel_id"], 'message.id' => $redis->get(getallheaders()['x-gk-event'] . ':' . $eventid), 'embed' => $embed]);
         }
     }
 
@@ -64,9 +45,10 @@
         exit;
     }
 
-    if (method_exists('event_router', getallheaders()['x-gk-event'])) {
-        [new event_router(), getallheaders()['x-gk-event']]();
-    } else {
-        [new event_router(), "invalid"]();
-    }
+    //figure out a way to save what events go wear with some type of update path between each webhook send
+
+
+    //get eventid
+
+
 ?>
